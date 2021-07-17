50beab3618cb1dfd347d135a9031be9aeb1767cc
Larisa Avtukhova <aln1967@yandex.ru>
2021-07-17 14:24:31 +0300

    Added method2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ваши бонусные мили:");
-        int price = 2568;
-        int amountmiles = price / 20;
+        int price = 1000;
+        int price_mile = 20;
+        int amountmiles = price / price_mile;
 
         System.out.println(amountmiles);
 
