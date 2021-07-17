954edf482ec8feef9d5c5eba0374c755a9d70a3a
Larisa Avtukhova <aln1967@yandex.ru>
2021-07-17 14:15:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ваши бонусные мили:");
+        int price = 2568;
+        int amountmiles = price / 20;
+
+        System.out.println(amountmiles);
+
+
+    }
+}
